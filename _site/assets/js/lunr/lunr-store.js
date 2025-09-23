64bec332309e483cf5c9d20b1cef1fa657cd66ca
anuragkhuntia <kanurag03@gmail.com>
2025-09-23 19:52:37 +0530

    fixed dependencies

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Automating Bare Metal with Terraform & MAAS",
+        "excerpt":"Provisioning servers manually is slow and error-prone.  In this post, I’ll share how I automated bare metal provisioning using:      Terraform for infrastructure as code   MAAS (Metal-as-a-Service) for managing hardware   Ansible for configuration management   The result: 70% faster deployments for UPI and IMPS workloads.   ","categories": [],
+        "tags": [],
+        "url": "/2025/09/01/first-post.html",
+        "teaser": null
+      },{
+        "title": "Kubernetes Networking Across On-Prem Datacenters with BGP, ECMP, and BFD",
+        "excerpt":"Introduction Kubernetes powers mission-critical applications, many of which run in on-premises datacenters spanning multiple sites. Unlike public clouds that abstract multi-region networking, on-premises Kubernetes deployments require native, scalable underlay fabrics to route Pod and Service IPs efficiently. Cloud providers simplify networking using managed services. However, in on-prem environments, architects must...","categories": [],
+        "tags": [],
+        "url": "/2025/09/21/k8s-bgp-ecmp-bfd.html",
+        "teaser": null
+      }]
